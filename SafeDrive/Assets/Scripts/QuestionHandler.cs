d196a8e266f3f249018d75c08f42c5cdb4e80617
winchell459 <johncmorris@gmail.com>
2020-09-24 16:09:37 -0700

    End of day 9/23/20

---
+++ SafeDrive/Assets/Scripts/QuestionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionHandler : MonoBehaviour
+{
+    public GameObject QuestionUI;
+    public Text QuestionText, DescriptionText, AnswerText;
+    public Question CurrentQuestion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayQuestion();
+    }
+
+    public void DisplayQuestion()
+    {
+        QuestionText.text = CurrentQuestion.MyQuestion;
+        DescriptionText.text = CurrentQuestion.Description;
+        string answers = "";
+        foreach(string answer in CurrentQuestion.Answers)
+        {
+            answers += answer + "\n";
+        }
+        AnswerText.text = answers;
+        QuestionUI.SetActive(true);
+    }
+    
+}
