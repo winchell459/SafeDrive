d210cf86d8a919170a31eb91bb5bac93e4e251fd
winchell459 <johncmorris@gmail.com>
2020-10-17 11:00:15 -0700

    end of day 10/16/20

---
+++ SafeDrive/Assets/Scripts/AICar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AICar : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    private int waypointIndex = 0;
+    private Vector3 direction;
+    private UnityStandardAssets.Vehicles.Car.CarController car;
+    public GameObject FrontLeftWheel, FrontRightWheel;
+    float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(Waypoints[waypointIndex].position);
+        direction = transform.forward;
+        car = GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(Waypoints[waypointIndex].position, transform.position) < 1)
+        {
+            GetComponent<NavMeshAgent>().speed = Waypoints[waypointIndex].GetComponent<AIMarker>().Speed;
+
+            waypointIndex += 1;
+            waypointIndex %= Waypoints.Length;
+            GetComponent<NavMeshAgent>().SetDestination(Waypoints[waypointIndex].position);
+
+            angle = Vector3.SignedAngle(transform.forward, Waypoints[waypointIndex].position - transform.position, transform.up);
+        }
+
+        //angle = Vector3.SignedAngle(transform.forward, direction, transform.up);
+        angle = Vector3.SignedAngle(transform.forward, Waypoints[waypointIndex].position - transform.position, transform.up);
+        FrontLeftWheel.transform.localEulerAngles = new Vector3(0, angle, 0);
+        FrontRightWheel.transform.localEulerAngles = new Vector3(0, angle, 0);
+
+        direction = transform.forward;
+    }
+}
---
+++ SafeDrive/Assets/Scripts/NavmeshAgentTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavmeshAgentTest : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Vector3 MoveTo;
+    private float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.Distance(transform.position, MoveTo));
+        if(Vector3.Distance(transform.position, MoveTo) < 1.0f)
+        {
+            MoveTo = new Vector3(Random.Range(-10, 10), 0.5f, Random.Range(-10, 10));
+        }
+        agent.SetDestination(MoveTo);
+
+    }
+}
