bd853644301ae2e5d11a6dc759adc29b00c65b13
charlesy2022 <68930343+charlesy2022@users.noreply.github.com>
2020-11-13 20:17:11 -0800

    11/13/2020
    completed RightTurnUnit and SchoolBusUnit (all units are finished)

--- Assets/Scripts/Events/TestEvent.cs
+++ Assets/Scripts/Events/TestEvent.cs
@@ -47,44 +47,43 @@ public class TestEvent : MonoBehaviour
             }
             if (areaDetector)
             {
-                if(areaDetector.Completed && areaDetector.Pass)
+                if(areaDetector.Completed && (areaDetector.EnterPass && areaDetector.Pass || !areaDetector.EnterPass && !areaDetector.Pass)) //enterPass && pass || !enterPass && !pass
                 {
-                    if (!eventsInitialized)
-                    {
-                        InitializeEvents();
-                    }
-                    if (isEventComplete() || (CheckEngine && !FindObjectOfType<DriverHandler>().EngineState()))
-                    {
-                        score = scoreEvent();
-                        EventCompleted = true;
-                    }
+                    handleEvents();
                 }
             }
             else
             {
-                /* bool complete = true;
+                handleEvents();
+            }
+        }
+    }
+    void handleEvents()
+    {
+        /* bool complete = true;
                  bool pass = true;
                  foreach(EventScript myEvent in events)  */
-                //(T || (...)) or (F || T && T)
-                if (!eventsInitialized)
-                {
-                    InitializeEvents();
-                }
-                if (isEventComplete() || (CheckEngine && !FindObjectOfType<DriverHandler>().EngineState()))
-                {
-                    score = scoreEvent();
-                    EventCompleted = true;
-                } 
-            }
+        //(T || (...)) or (F || T && T)
+        if (!eventsInitialized)
+        {
+            InitializeEvents();
+        }
+        if (isEventComplete() || (CheckEngine && !FindObjectOfType<DriverHandler>().EngineState()))
+        {
+            score = scoreEvent();
+            EventCompleted = true;
         }
     }
     private void InitializeEvents()
     {
+        //initializes detectors
         foreach (EventScript myEvent in events)
         {
             if (myEvent.EventType != EventScript.EventTypes.Area || myEvent != areaDetector) myEvent.Initialize();
         }
         eventsInitialized = true;
+
+        //initializes next events
         foreach(TestEvent nextEvent in NextEvents)
         {
             nextEvent.Initialized = true;
@@ -157,6 +156,7 @@ public class TestEvent : MonoBehaviour
         }
         card.Score = (int)score;
         card.Total = (int)total;
+        Debug.Log(transform.name + " score:" + card.Score + "total: " + card.Total);
         return card;
     }
 
